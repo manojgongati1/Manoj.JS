97c899a5c1a09f2ce0a677d44f0ee13a80b41f95
manojgongati1 <68334486+manojgongati1@users.noreply.github.com>
2024-02-20 23:51:05 -0600

    Create basicfunction.js

---
+++ basicfunction.js
@@ -0,0 +1,7 @@
+function legend(champion,origin,weapon){
+  var string="";
+  string = "Mighty "+champion+" from "+origin+" attacks with the "+weapon;
+  return string;
+  
+}
+console.log(legend("Garen","Demacia","Longsword"))
