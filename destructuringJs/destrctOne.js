2aea62e2397553352f2f0b53a218e5d8329580d2
manojgongati1 <68334486+manojgongati1@users.noreply.github.com>
2024-02-25 20:26:14 -0600

    Create destrctOne.js

---
+++ destructuringJs/destrctOne.js
@@ -0,0 +1,36 @@
+const champions = [
+  {
+    "name": "Garen",
+    "weapon": "Greatsword",
+    "location": "Demacia",
+    "power": "Strength and Courage"
+  },
+  {
+    "name": "Ahri",
+    "weapon": "Fox Magic",
+    "location": "Ionia",
+    "power": "Charm and Spirit Orbs"
+  },
+  {
+    "name": "Ashe",
+    "weapon": "Ice Bow",
+    "location": "Freljord",
+    "power": "Ice Arrows and Leadership"
+  },
+  {
+    "name": "Zed",
+    "weapon": "Shadow Techniques",
+    "location": "Ionia",
+    "power": "Shadow Magic and Assassination"
+  },
+  {
+    "name": "Miss Fortune",
+    "weapon": "Dual Pistols",
+    "location": "Bilgewater",
+    "power": "Gunplay and Strategic Wit"
+  }
+];
+
+var {weapon : bestWeapon } = champions[3]
+
+console.log(bestWeapon)
